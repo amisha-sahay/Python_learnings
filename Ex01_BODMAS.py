ee42baa3cc1903be67f91d61e225f4e1f14b6bce
amisha-sahay <amishasahaywork@gmail.com>
2026-09-02 05:58:14 +0530

    Add files via upload

---
+++ Ex01_BODMAS.py
@@ -0,0 +1,17 @@
+#BODMAS and Arithmetic Operations:
+op1 = 48/6*(3+5)-2**3
+op2 = (25-10)/3-100/(5+5)
+op3 = 2**5+10-56
+op4 = (4+2-3)**2
+op5 = 100000/4*15*21
+op6 = 876//2
+op7 = 755%10
+print("Output for operation 1 =", op1)
+print("Output for operation 2 =", op2)
+print("Output for operation 3 =", op3)
+print("Output for operation 4 =", op4)
+print("Output for operation 5 =", op5)
+print("Output for operation 6 =", op6)
+print("Output for operation 7 =", op7)
+
+#I learned to perform basic BODMAS operations through coding and printing their outputs.
